794c8bb0006633b5311a58cb56ad1768358fa9ed
Dixyee <enache.robert93@gmail.com>
2023-02-23 18:52:21 +0200

    ForwardAndBack
    +Added Platform Move forward And back

--- Source/PuzzlePlatform/MovingPlatform.cpp
+++ Source/PuzzlePlatform/MovingPlatform.cpp
@@ -18,16 +18,35 @@ void AMovingPlatform::BeginPlay()
 		SetReplicates(true);
 		SetReplicateMovement(true);
 	}
+
+	GlobalStartLocation = GetActorLocation();//Point A
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);//Point B
 }
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (HasAuthority()!=true)
-	{
-		FVector Location = GetActorLocation();
-		Location += FVector(speed * DeltaTime, 0, 0);
+	if (HasAuthority())
+	{	
+		
+		FVector Location = GetActorLocation(); // Point C 
+		float JourneyLenght = (GlobalTargetLocation - GlobalStartLocation).Size(); //The lenght From point B to A
+		float JourneyTravelled = (Location - GlobalStartLocation).Size(); //The lengt from point C to A
+		
+		// if the lenght of C to A is bigger than B to A	
+		//		then swap B and A
+
+		if (JourneyTravelled >= JourneyLenght) 
+		{
+			FVector Swap = GlobalStartLocation;
+			GlobalStartLocation = GlobalTargetLocation;
+			GlobalTargetLocation = Swap;
+		}
+
+		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+		Location += speed * DeltaTime * Direction;
 		SetActorLocation(Location);
+
 	}
 
 }
